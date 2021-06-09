99d8434600629655e85b91a369fa655e36eafe15
jzx <945627077@qq.com>
2021-06-09 12:18:13 +0800

    'update'

---
+++ src/cmake-build-debug/CMakeFiles/test_antlr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_antlr.dir/main.cpp.obj"
+  "test_antlr.exe"
+  "test_antlr.exe.manifest"
+  "test_antlr.lib"
+  "test_antlr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_antlr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "antlr4-runtime.h"
+using  std::cout;
+#include <Windows.h>
+#pragma execution_character_set("utf-8")
+
+using namespace antlr4;
+int main(){
+    cout<<"start=======";
+    return 0;
+}
